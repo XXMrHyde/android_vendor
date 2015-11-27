b97e071b8713f24829fdca38ed054df7af7dc88a
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-11-27 21:07:45 +0100

    shamu: Remove ProjectFi

--- motorola/shamu/device-vendor.mk
+++ motorola/shamu/device-vendor.mk
@@ -21,7 +21,6 @@ PRODUCT_PACKAGES += \
     HiddenMenu \
     ims \
     TimeService \
-    Tycho \
     VZWAPNLib
 
 # Prebuilt jars
@@ -42,7 +41,6 @@ PRODUCT_PACKAGES += \
     DCMO \
     DiagMon \
     DMService \
-    GCS \
     LifetimeData \
     MotoSignatureApp \
     SprintDM \
